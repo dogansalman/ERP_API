d1814e50fa80c9a4f9aca1b14c52dd23b6d1056b
dogansalman <dogan.salman@yahoo.com>
2017-10-17 17:27:39 +0300

    update

--- abkar_api/Migrations/Configuration.cs
+++ abkar_api/Migrations/Configuration.cs
@@ -11,7 +11,7 @@ namespace abkar_api.Migrations
     
     internal sealed class Configuration : DbMigrationsConfiguration<abkar_api.Models.MigrationContexts>
     {
-        bool EnableDummyData = false;
+        bool EnableDummyData = true;
 
         public Configuration()
         {
