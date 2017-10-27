5b7fbb5ea6a6ffad399e826acb5c2f1d71e4c331
dogansalman <dogan.salman@yahoo.com>
2017-10-27 17:48:00 +0300

    update

--- abkar_api/Controllers/ProductionsController.cs
+++ abkar_api/Controllers/ProductionsController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using abkar_api.Models;
 using abkar_api.Libary.ExceptionHandler;
 using abkar_api.Contexts;
+using System.Data.Entity.SqlServer;
 
 namespace abkar_api.Controllers
 {
@@ -14,6 +15,22 @@ namespace abkar_api.Controllers
     public class ProductionsController : ApiController
     {
         DatabaseContext db = new DatabaseContext();
+
+        [HttpGet]
+        [Route("{id}")]
+        public object gets(int id)
+        {
+            return (
+                from p in db.productions
+                where p.order_id == id
+                join o in db.orders on p.order_id equals o.id
+                select new
+                {
+                    production = p,
+                    order = o,
+                }
+                ).ToList();
+        }
 
         [Route("")]
         [HttpPost]
@@ -65,6 +82,43 @@ namespace abkar_api.Controllers
 
             return Ok(production);
 
+        }
+
+        [Route("detail/{id}")]
+        [HttpGet]
+        public object get(int id)
+        {
+            
+            return (
+                from pro in db.productions
+                where pro.id == id
+            select new
+                {
+                    description = pro.description,
+                    name = pro.name,
+                    end_time = pro.end_time.ToString().Substring(0, 5),
+                    start_time = pro.start_time.ToString().Substring(0, 5),
+                    unit = pro.unit,
+                    is_cancel = pro.is_cancel,
+                    is_complate = pro.is_complate,
+                    order_id = pro.order_id,
+                    production_personnels = (
+                    from proper in db.production_personnels
+                    join per in db.personnels on proper.personel_id equals per.id
+                    where proper.production_id == id
+                    select new
+                    {
+                        personnel = per,
+                        operations = (
+                         from ppo in db.production_personnel_operation
+                         join pp in db.production_personnels on ppo.production_personel_id equals pp.id 
+                         where pp.personel_id == per.id
+                         select new {ppo}
+                        ).ToList()
+                    }
+                    ).ToList()
+                }
+                ).FirstOrDefault();
         }
     }
 }
